4ee51413b476a37815274066af1f97ca91ca46a1
waelb2 <lw_bouguessa@esi.dz>
2024-07-13 14:29:51 +0100

    code refactoring

--- main.ts
+++ main.ts
@@ -8,18 +8,18 @@ const server = net.createServer((socket) => {
   socket.on("data", async (bufferRequest) => {
     const httpRequest = bufferRequest.toString("utf8");
     const headers: string[] = httpRequest.split("\r\n");
-    const requestLine: string = headers[0];
-    const URLPath: string = requestLine.split(" ")[1];
-    const method: string = requestLine.split(" ")[0];
+    const requestLineChunks: string[] = headers[0].split(" ");
+    const URLPath: string = requestLineChunks[1];
+    const method: string = requestLineChunks[0].toLowerCase().trim();
     const query: string = URLPath.split("/")[2];
 
-    let response: string = "";
-    let body: Buffer | string = "";
     try {
-      if (method.toLowerCase().trim() === "get") {
+      let response: string = "";
+      let body: Buffer | string = "";
+      if (method === "get") {
         // handling GET request
         [response, body] = await handleGetMethod(URLPath, query, headers);
-      } else if (method.toLowerCase().trim() === "post") {
+      } else if (method === "post") {
         // handling POST request
         const postBody: string = headers[headers.length - 1];
         response = postBody
@@ -31,13 +31,17 @@ const server = net.createServer((socket) => {
       if (body.length !== 0) {
         socket.write(body);
       }
-      socket.end();
     } catch (error) {
       const serverErrorResponse: string = HTTP_STATUS_SERVER_ERROR + endString;
       socket.write(serverErrorResponse);
-      socket.end();
       console.log(error);
+    } finally {
+      socket.end();
     }
+  });
+  socket.on("error", (err) => {
+    console.error(err);
+    socket.end();
   });
   socket.on("close", () => {
     socket.end();
