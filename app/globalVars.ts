2650573a4214ace21cd3ee9ef5e35666a1b30026
waelb2 <lw_bouguessa@esi.dz>
2024-07-13 14:23:26 +0100

    code refactoring

--- app/globalVars.ts
+++ app/globalVars.ts
@@ -1,5 +1,6 @@
 export const HTTP_STATUS_CREATED: string = `HTTP/1.1 201 Created\r\n`;
 export const HTTP_STATUS_OK: string = `HTTP/1.1 200 OK\r\n`;
+export const HTTP_STATUS_SERVER_ERROR: string = `HTTP/1.1 500 Internal Server Error\r\n`;
 export const HTTP_STATUS_NOT_FOUND: string = `HTTP/1.1 404 Not Found\r\n`;
 export const CONTENT_TYPE_PLAIN: string = `Content-Type: text/plain\r\n`;
 export const CONTENT_TYPE_APP: string = `Content-Type: application/octet-stream\r\n`;
--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
 import * as net from "net";
 import handleGetMethod from "./app/handleGetMethod";
 import handlePostRequest from "./app/handlePostRequest";
+import { HTTP_STATUS_SERVER_ERROR } from "./app/globalVars";
+import { endString } from "./app/globalVars";
+
 const server = net.createServer((socket) => {
   socket.on("data", async (bufferRequest) => {
     const httpRequest = bufferRequest.toString("utf8");
-    const headers = httpRequest.split("\r\n");
-    const requestLine = headers[0];
+    const headers: string[] = httpRequest.split("\r\n");
+    const requestLine: string = headers[0];
     const URLPath: string = requestLine.split(" ")[1];
     const method: string = requestLine.split(" ")[0];
     const query: string = URLPath.split("/")[2];
@@ -18,22 +21,23 @@ const server = net.createServer((socket) => {
         [response, body] = await handleGetMethod(URLPath, query, headers);
       } else if (method.toLowerCase().trim() === "post") {
         // handling POST request
-
-        const body: string = headers[headers.length - 1];
-
-        response = body ? await handlePostRequest(URLPath, query, body) : "";
+        const postBody: string = headers[headers.length - 1];
+        response = postBody
+          ? await handlePostRequest(URLPath, query, body)
+          : "";
       }
 
       socket.write(response);
-
       if (body.length !== 0) {
         socket.write(body);
       }
+      socket.end();
     } catch (error) {
+      const serverErrorResponse: string = HTTP_STATUS_SERVER_ERROR + endString;
+      socket.write(serverErrorResponse);
+      socket.end();
       console.log(error);
     }
-
-    socket.end();
   });
   socket.on("close", () => {
     socket.end();
